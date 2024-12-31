c9c481927af496f69491cbfd61b41b53f80a3b60
Denis Kolokolov <dennis.kolokolov@gmail.com>
2024-12-31 12:59:45 +0100

    feat: add description field to courses and display in home view

--- tests/Feature/PageHomeTest.php
+++ tests/Feature/PageHomeTest.php
@@ -8,15 +8,19 @@ uses(RefreshDatabase::class);
 
 it('shows courses overview', function () {
     // Arrange
-    Course::factory()->create(['title' => 'Course A']);
-    Course::factory()->create(['title' => 'Course B']);
-    Course::factory()->create(['title' => 'Course C']);
+    Course::factory()->create(['title' => 'Course A', 'description' => 'Description Course A']);
+    Course::factory()->create(['title' => 'Course B', 'description' => 'Description Course B']);
+    Course::factory()->create(['title' => 'Course C', 'description' => 'Description Course C']);
 
     // Act & Assert
     get(route('home'))
         ->assertSeeText([
             'Course A',
+            'Description Course A',
             'Course B',
+            'Description Course B',
+            'Course C',
+            'Description Course C'
         ]);
 });
 
